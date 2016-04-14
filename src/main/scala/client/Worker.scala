63e0b52c9ef0984b03e86fc68ab41087ec91f5cf
Tomek Godzik <tgodzik@virtuslab.com>
2016-04-14 11:25:17 +0200

    Added work parallelization

---
+++ src/main/scala/client/Worker.scala
@@ -0,0 +1,37 @@
+package client
+
+import akka.actor.{Actor, Props}
+import com.virtuslab.akkaworkshop.Decrypter
+import com.virtuslab.akkaworkshop.PasswordsDistributor.{ValidateDecodedPassword, EncryptedPassword}
+
+import scala.util.Try
+
+class Worker extends Actor {
+
+  var decrypter = new Decrypter
+
+  private def decryptPassword(password: String): Try[String] = Try {
+    val prepared = decrypter.prepare(password)
+    val decoded = decrypter.decode(prepared)
+    val decrypted = decrypter.decrypt(decoded)
+    decrypted
+  }
+
+  override def receive: Receive = working
+
+  def working : Receive = {
+    case ep@EncryptedPassword(encryptedPassword) =>
+      val decrypted = decryptPassword(encryptedPassword)
+      decrypted.map {
+        decryptedPassword =>
+          sender ! ValidateDecodedPassword("", encryptedPassword, decryptedPassword)
+      }.getOrElse{
+        decrypter = new Decrypter
+        self forward ep
+      }
+  }
+}
+
+object Worker {
+  def props = Props[Worker]
+}

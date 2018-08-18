ff5a9837423f6b38702c66c9730594518929f435
Paweł Marks <pmarks@virtuslab.com>
2018-08-18 20:13:32 +0200

    Before step 4

--- src/main/kotlin/coroutines/workshop/app.kt
+++ src/main/kotlin/coroutines/workshop/app.kt
@@ -6,12 +6,11 @@ import kotlinx.coroutines.experimental.channels.Channel
 import kotlinx.coroutines.experimental.channels.ReceiveChannel
 import kotlinx.coroutines.experimental.channels.SendChannel
 import kotlinx.coroutines.experimental.channels.produce
-import kotlinx.coroutines.experimental.selects.select
 import kotlin.coroutines.experimental.CoroutineContext
 
 // Class representing finished decryption.
 // It is intended to being used to send data from worker coroutines.
-data class Decryption(val decrypter: Decrypter, val input: String, val output: String, val parent: Job)
+data class Decryption(val decrypter: Decrypter, val input: String, val output: String)
 
 // Coroutine context containing only thread pool of fixed size.
 val executor: CoroutineContext by lazy {
@@ -25,28 +24,22 @@ fun main(args: Array<String>) = runBlocking {
 
     val newPasswords = api.passwords(token)
 
-    var allDecryptions: Job? = null
-
     lateinit var finishedDecryptions: Channel<Decryption>
 
     val reset = suspend {
-        allDecryptions?.cancel()
         finishedDecryptions = Channel(Decrypter.maxClientCount)
-        allDecryptions = Job().also { parent ->
-            repeat(Decrypter.maxClientCount) {
-                Decrypter().process(newPasswords.receive(), parent, finishedDecryptions)
-            }
+        repeat(Decrypter.maxClientCount) {
+            Decrypter().process(newPasswords.receive(), finishedDecryptions)
         }
     }
 
     reset()
 
     while (isActive) {
-        for ((decrypter, input, output, parent) in finishedDecryptions) {
-            decrypter.process(newPasswords.receive(), parent, finishedDecryptions)
+        for ((decrypter, input, output) in finishedDecryptions) {
+            decrypter.process(newPasswords.receive(), finishedDecryptions)
             api.validate(Validate(token, input, output))
         }
-        reset()
     }
 }
 
@@ -61,14 +54,8 @@ suspend fun Api.passwords(token: String): ReceiveChannel<String> = produce {
 
 suspend fun Decrypter.process(
         password: String,
-        parent: Job,
         answerChannel: SendChannel<Decryption>
-) = launch(executor, parent = parent) {
-    try {
-        val output = decrypt(decode(prepare(password)))
-        answerChannel.send(Decryption(this@process, password, output, parent))
-    } catch (e: Throwable) {
-        println("$e\n")
-        if (!answerChannel.isClosedForSend) answerChannel.close()
-    }
+) = launch(executor) {
+    val output = decrypt(decode(prepare(password)))
+    answerChannel.send(Decryption(this@process, password, output))
 }

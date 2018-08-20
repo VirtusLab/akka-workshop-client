f9652da521aed04d6dd7c9f012e4cfc7a3a56c4c
Krzysztof Borowski <kborowski@virtuslab.com>
2018-08-20 23:33:10 +0200

    PasswordClient http protocol with akka-http

--- build.sbt
+++ build.sbt
@@ -4,10 +4,13 @@ version := "2.0"
 
 scalaVersion := "2.12.5"
 
-val akkaVersion = "2.5.11"
+val akkaVersion     = "2.5.14"
+val akkaHttpVersion = "10.1.3"
 
 libraryDependencies ++= Seq(
-  "commons-codec" % "commons-codec" % "1.11",
-  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
-  "com.typesafe.akka" %% "akka-remote" % akkaVersion
+  "commons-codec"     % "commons-codec"         % "1.11",
+  "com.typesafe.akka" %% "akka-actor"           % akkaVersion,
+  "com.typesafe.akka" %% "akka-remote"          % akkaVersion,
+  "com.typesafe.akka" %% "akka-http"            % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion
 )
---
+++ src/main/scala/client/PasswordClient.scala
@@ -0,0 +1,66 @@
+package client
+import scala.concurrent.Future
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.unmarshalling.Unmarshaller
+import akka.stream.ActorMaterializer
+import com.virtuslab.akkaworkshop.PasswordsDistributor._
+import spray.json._
+
+class PasswordClient()(implicit actorSystem: ActorSystem) extends JsonSupport {
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = actorSystem.dispatcher
+
+  def requestToken(register: Register): Future[Registered] = {
+    Http()
+      .singleRequest(
+        HttpRequest(
+          HttpMethods.POST,
+          "http://localhost:9000/register",
+          entity = HttpEntity(ContentTypes.`application/json`, register.toJson.toString())
+        )
+      )
+      .flatMap(r => Unmarshaller.stringUnmarshaller(r.entity).map(_.asJson.convertTo[Registered]))
+  }
+
+  def requestPassword(sendMeEncryptedPassword: SendMeEncryptedPassword): Future[EncryptedPassword] = {
+    Http()
+      .singleRequest(
+        HttpRequest(
+          HttpMethods.POST,
+          "http://localhost:9000/send-encrypted-password",
+          entity = HttpEntity(ContentTypes.`application/json`, sendMeEncryptedPassword.toJson.toString())
+        )
+      )
+      .flatMap(r => Unmarshaller.stringUnmarshaller(r.entity).map(_.asJson.convertTo[EncryptedPassword]))
+  }
+
+  def validatePassword(validatePassword: ValidateDecodedPassword): Future[PDMessageResponse] = {
+    Http()
+      .singleRequest(
+        HttpRequest(
+          HttpMethods.POST,
+          "http://localhost:9000/validate",
+          entity = HttpEntity(ContentTypes.`application/json`, validatePassword.toJson.toString())
+        )
+      )
+      .flatMap {
+        case r if r.status == StatusCodes.OK =>
+          Unmarshaller.stringUnmarshaller(r.entity).map(_.asJson.convertTo[PasswordCorrect])
+        case r => Unmarshaller.stringUnmarshaller(r.entity).map(_.asJson.convertTo[PasswordIncorrect])
+      }
+  }
+}
+
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val registerFormat: JsonFormat[Register]                               = jsonFormat1(Register)
+  implicit val registeredFormat: JsonFormat[Registered]                           = jsonFormat1(Registered)
+  implicit val sendMeEncryptedPasswordFormat: JsonFormat[SendMeEncryptedPassword] = jsonFormat1(SendMeEncryptedPassword)
+  implicit val encryptedPasswordFormat: JsonFormat[EncryptedPassword]             = jsonFormat1(EncryptedPassword)
+  implicit val passwordCorrectFormat: JsonFormat[PasswordCorrect]                 = jsonFormat1(PasswordCorrect)
+  implicit val passwordIncorrectFormat: JsonFormat[PasswordIncorrect]             = jsonFormat2(PasswordIncorrect)
+  implicit val validatePasswordFormat: JsonFormat[ValidateDecodedPassword]        = jsonFormat3(ValidateDecodedPassword)
+}

272aa1e5d0773864a748171ffe79b67439bee499
Krzysztof Borowski <kborowski@virtuslab.com>
2018-08-20 23:33:10 +0200

    PasswordClient http protocol with akka-http

--- build.sbt
+++ build.sbt
@@ -4,12 +4,13 @@ version := "2.0"
 
 scalaVersion := "2.12.5"
 
-resolvers += "Workshop Repository" at "http://headquarters:8081/artifactory/libs-release-local"
-
-val akkaVersion = "2.5.11"
+val akkaVersion     = "2.5.14"
+val akkaHttpVersion = "10.1.3"
 
 libraryDependencies ++= Seq(
-  "commons-codec" % "commons-codec" % "1.11",
-  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
-  "com.typesafe.akka" %% "akka-remote" % akkaVersion
+  "commons-codec"     % "commons-codec"         % "1.11",
+  "com.typesafe.akka" %% "akka-actor"           % akkaVersion,
+  "com.typesafe.akka" %% "akka-remote"          % akkaVersion,
+  "com.typesafe.akka" %% "akka-http"            % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion
 )
--- src/main/scala/client/RequesterActor.scala
+++ src/main/scala/client/RequesterActor.scala
@@ -4,10 +4,11 @@ import akka.actor._
 import akka.routing.RoundRobinPool
 import com.virtuslab.akkaworkshop.Decrypter
 import com.virtuslab.akkaworkshop.PasswordsDistributor._
+import akka.pattern.pipe
 
 import scala.util.Try
 
-class RequesterActor(remote: ActorRef) extends Actor with ActorLogging {
+class RequesterActor(remote: PasswordClient) extends Actor with ActorLogging {
 
   var decrypter = new Decrypter
   val name      = "Cesar"
@@ -15,6 +16,7 @@ class RequesterActor(remote: ActorRef) extends Actor with ActorLogging {
   val workersNumber = 5
   val workers =
     context.actorOf(RoundRobinPool(workersNumber).props(Worker.props))
+  implicit val ec = context.dispatcher
 
   private def decryptPassword(password: String): Try[String] = Try {
     val prepared  = decrypter.prepare(password)
@@ -24,15 +26,15 @@ class RequesterActor(remote: ActorRef) extends Actor with ActorLogging {
   }
 
   override def preStart(): Unit =
-    remote ! Register(name)
+    remote.requestToken(Register(name)).pipeTo(self)
 
   override def receive: Receive = starting
 
   def starting: Receive = {
     case Registered(token) =>
       log.info(s"Registered with token $token")
-      remote ! SendMeEncryptedPassword(token)
-      for (_ <- 0 until workersNumber) remote ! SendMeEncryptedPassword(token)
+      remote.requestPassword(SendMeEncryptedPassword(token)).pipeTo(self)
+      for (_ <- 0 until workersNumber) remote.requestPassword(SendMeEncryptedPassword(token)).pipeTo(self)
       context.become(working(token))
   }
 
@@ -41,21 +43,21 @@ class RequesterActor(remote: ActorRef) extends Actor with ActorLogging {
       workers ! encryptedPassword
 
     case ValidateDecodedPassword(_, encrypted, decrypted) =>
-      remote ! ValidateDecodedPassword(token, encrypted, decrypted)
+      remote.validatePassword(ValidateDecodedPassword(token, encrypted, decrypted)).pipeTo(self)
 
     case PasswordCorrect(decryptedPassword) =>
       log.info(s"Password $decryptedPassword was decrypted successfully")
-      remote ! SendMeEncryptedPassword(token)
+      remote.requestPassword(SendMeEncryptedPassword(token)).pipeTo(self)
 
     case PasswordIncorrect(decryptedPassword, correctPassword) =>
       log.error(s"Password $decryptedPassword was not decrypted correctly, should be $correctPassword")
-      remote ! SendMeEncryptedPassword(token)
+      remote.requestPassword(SendMeEncryptedPassword(token)).pipeTo(self)
   }
 
 }
 
 object RequesterActor {
 
-  def props(remote: ActorRef) = Props(new RequesterActor(remote))
+  def props(remote: PasswordClient) = Props(new RequesterActor(remote))
 
 }

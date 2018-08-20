a2514ad56a3e3348eb1b469c4e0f947ab6bfd009
Piotr Gawryś <pgawrys2@gmail.com>
2018-08-20 10:33:00 +0200

    Remove extra lines from checkCancel

--- src/main/scala/client/Decrypting.scala
+++ src/main/scala/client/Decrypting.scala
@@ -18,8 +18,6 @@ object Decrypting {
 
     def checkCancel(): IO[Unit] =
       for {
-        shouldStop <- cancelSignal.get
-        result <- if (shouldStop) passwordQueue.put(password) *> IO.raiseError(CancelException) else IO.unit
         // This is needed to achieve 100% correctness.
         // Sometimes one thread can fail and before it can recover from error the other one can go to the next stage
         // with incorrect result so we're introducing short (non-blocking) delay to account for that.

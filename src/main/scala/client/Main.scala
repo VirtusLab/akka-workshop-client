54444df96693621a2b6dd254ce1dd73f8ff59581
Piotr Gawryś <pgawrys2@gmail.com>
2018-08-20 10:04:14 +0200

    Move getPassword

--- src/main/scala/client/Main.scala
+++ src/main/scala/client/Main.scala
@@ -37,7 +37,7 @@ object Main extends IOApp {
     ???
   }
 
-  def getPassword[F[+_]](client: PasswordClient[F], token: Token): F[Password] = {
+  def getPassword(client: PasswordClient[IO], token: Token): IO[Password] = {
     client.requestPassword(token)
   }
 }

41f523ac1314835a1c7dea79548f24ecd1890c5a
Krzysztof Borowski <kborowski@virtuslab.com>
2018-08-21 00:23:20 +0200

    Use PasswordClient password decryptor over http

--- src/main/scala/client/AkkaApplication.scala
+++ src/main/scala/client/AkkaApplication.scala
@@ -4,28 +4,12 @@ import akka.actor._
 import akka.util.Timeout
 
 import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
 
 object AkkaApplication extends App {
 
   implicit val timeout = Timeout(10.seconds)
 
-  val system = ActorSystem("RequesterSystem")
+  implicit val system = ActorSystem("RequesterSystem")
 
-  val remoteIp   = "headquarters"
-  val remotePort = 9552
-
-  val remoteServer = system.actorSelection(s"akka.tcp://application@$remoteIp:$remotePort/user/PasswordsDistributor")
-
-  remoteServer
-    .resolveOne()
-    .map { remoteServerRef =>
-      system.actorOf(RequesterActor.props(remoteServerRef), name = "requester")
-    }
-    .failed
-    .foreach { error =>
-      println(s"Unable to establish connection to $remoteIp:$remotePort; ${error.getMessage}")
-      system.terminate()
-    }
-
+  system.actorOf(RequesterActor.props(new PasswordClient()), name = "requester")
 }

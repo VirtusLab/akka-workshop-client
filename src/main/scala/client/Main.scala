349b25ec0fb56c6b8eed99d97b9fe29657038dfe
Piotr Gawryś <pgawrys2@gmail.com>
2018-08-20 09:06:56 +0200

    Refactor PasswordClient

--- src/main/scala/client/Main.scala
+++ src/main/scala/client/Main.scala
@@ -4,12 +4,8 @@ import java.util.concurrent._
 
 import cats.effect.{ExitCode, IO, IOApp, Timer}
 import com.virtuslab.akkaworkshop.Decrypter
-import org.http4s.client.Client
 import org.http4s.client.blaze.Http1Client
-import client.Decrypting._
-import client.PasswordClient._
-import cats.instances.list._
-import cats.syntax.all._
+
 import scala.concurrent.ExecutionContext
 
 object Main extends IOApp {
@@ -18,24 +14,25 @@ object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] =
     Http1Client[IO]()
-      .bracket { client =>
+      .bracket { httpClient =>
+        val client = PasswordClient.create(httpClient)
         for {
-          token <- requestToken("Piotrek")(client)
-          _ <- decryptForever(token)(client, timer)
+          token <- client.requestToken("Piotrek")
+          _ <- decryptForever(client, token)(timer)
         } yield ExitCode.Success
       }(_.shutdown)
 
-  def decryptForever(token: Token)(implicit httpClient: Client[IO], timer: Timer[IO]): IO[Unit] = {
+  def decryptForever(client: PasswordClient[IO], token: Token)(implicit timer: Timer[IO]): IO[Unit] = {
     val decrypter = new Decrypter
 
     // Notice that if you don't handle exceptions from Decrypter
     // they will propagate to this point failing the program.
     // What should we do in this case?
-    decryptingLoop(token, decrypter)
+    decryptingLoop(client, token, decrypter)
   }
 
-  def decryptingLoop(token: Token, decrypter: Decrypter)
-                    (implicit httpClient: Client[IO], timer: Timer[IO]): IO[Unit] = {
+  def decryptingLoop(client: PasswordClient[IO], token: Token, decrypter: Decrypter)
+                    (implicit timer: Timer[IO]): IO[Unit] = {
     // TODO: this `IO` should request, encrypt and validate passwords in infinite loop
     ???
   }

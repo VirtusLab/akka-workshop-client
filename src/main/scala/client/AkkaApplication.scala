1e7088ac730f7628057028cd2c47bb425476dd2c
Krzysztof Borowski <kborowski@virtuslab.com>
2018-08-21 21:53:43 +0200

    remove remote actor usage

--- src/main/scala/client/AkkaApplication.scala
+++ src/main/scala/client/AkkaApplication.scala
@@ -4,29 +4,12 @@ import akka.actor._
 import akka.util.Timeout
 
 import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
 
 object AkkaApplication extends App {
 
   implicit val timeout = Timeout(10.seconds)
 
   implicit val system = ActorSystem("RequesterSystem")
-
-  val remoteIp   = "headquarters"
-  val remotePort = 9552
-
-  val remoteServer = system.actorSelection(s"akka.tcp://application@$remoteIp:$remotePort/user/PasswordsDistributor")
-
-  remoteServer
-    .resolveOne()
-    .map { remoteServerRef =>
-
-    }
-    .failed
-    .foreach { error =>
-      println(s"Unable to establish connection to $remoteIp:$remotePort; ${error.getMessage}")
-      system.terminate()
-    }
 
   system.actorOf(RequesterActor.props(new PasswordClient()), name = "requester")
 

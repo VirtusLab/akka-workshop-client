a0f064c0a941a6e9b09b62ed4b286ce2f6649649
Krzysztof Romanowski <romanowski.kr@gmail.com>
2018-08-22 12:06:54 +0200

    Fixes for new server

--- src/main/scala/client/RequesterActor.scala
+++ src/main/scala/client/RequesterActor.scala
@@ -20,7 +20,7 @@ class RequesterActor(remote: PasswordClient) extends Actor with ActorLogging {
   }
 
   override def preStart(): Unit =
-    remote.requestToken(Register(name)).pipeTo(self)
+    remote.requestToken(name).pipeTo(self)
 
   override def receive: Receive = starting
 
